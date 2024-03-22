2bf7b3ca1cda5df36319f1d3c008d51c0dbbea05
Lais Vitoria <152716115+Lais-Vitoria@users.noreply.github.com>
2024-03-22 15:51:16 -0300

    Algoritimo01.c

--- algoritimo01.c
+++ algoritimo01.c
@@ -1,8 +1,7 @@
-//Algoritmo 28 do livro introdução a Progamação
 #include <stdio.h>
 #include <locale.h>
 
 int main () {
     setlocale(LC_ALL, "Portuguese");
-    printf("\nÉ PRECISO FAZER TODOS OS ALGORITMOS PARA APRENDER\n");
+    printf("\nÃ‰ PRECISO FAZER TODOS OS ALGORITMOS PARA APRENDER\n");
 }
