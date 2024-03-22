71f8c05b91c5f3d0246fa7ffa89c0cf56c189aa5
Lais Vitoria <152716115+Lais-Vitoria@users.noreply.github.com>
2024-03-22 15:46:13 -0300

    Add files via upload

---
+++ Algoritmo13.c
@@ -0,0 +1,33 @@
+// Algoritmo 40 do livro introdução á Progamação
+#include <stdio.h>
+#include <locale.h>
+
+int main ()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int Num1, Num2, Divisao, Resto;
+// O Primeiro número e o Dividendo
+    printf("\nEntre com um número inteiro: ");
+    scanf("%d",&Num1);
+// O segundo número e o Divisor
+    printf("\nEntre com o segunndo número inteiro: ");
+    scanf("%d",&Num2);
+// Divisao e o Quociente
+    Divisao = Num1 / Num2;
+// Resto da Divisão
+    Resto = Num1 % Num2;
+
+    printf("\nDividendo: %d", Num1);
+    printf("\nDivisor: %d", Num2);
+    printf("\nQuociente: %d", Divisao);
+    printf("\nResto: %d", Resto);
+
+
+}
+
+// As saidas devem ser:
+// dividendo
+// divisor
+// quociente
+// resto
