7535063dcf6c18adfda3b646b7fba5f8720dce7c
Lais Vitoria <152716115+Lais-Vitoria@users.noreply.github.com>
2024-03-23 22:00:46 -0300

    new

--- 04Media.c
+++ 04Media.c
@@ -6,5 +6,5 @@ int main ()
     float Media;
     Media = (8 + 9 + 7) / 3;
     setlocale(LC_ALL, "Portuguese");
-    printf("\nA Média aritmética de 8, 9 e 7 e: %.1f", Media);
+    printf("\nA MÃ©dia aritmÃ©tica de 8, 9 e 7 e: %.1f", Media);
 }
