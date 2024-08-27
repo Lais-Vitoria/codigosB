f9bd2fa417377c16007c58b3e15c18fca03f3dd4
Lais Vitoria Da Silva Cruz <laisvitoriadasilvacruz@gmail.com>
2024-08-27 18:28:03 -0300

    Função que imprime uma linha com o caracter "*".

---
+++ 4-Função/Linha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+void Linha( )
+{
+    printf("************************");
+}
+
+int main()
+{
+    char Caracter;
+
+    Linha(Caracter);
+    printf("\nCMP 1048\n");
+    Linha(Caracter);
+    printf("\nTÉCNICAS DE PROGAMAÇÃO\n");
+    Linha(Caracter);
+    
+    return 0;
+}
